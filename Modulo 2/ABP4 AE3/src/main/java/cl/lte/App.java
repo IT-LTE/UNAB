8a360c67d18d048707d681dcc3b3294bda0c0a8c
Alejandra Flaño <IT-LTE@latiaencanto.cl>
2025-07-22 14:47:16 -0400

    12.- Se Desarrolla codigo de ejercicio ingreso de alumno, sin saber cuantos, calcula prodeio curso, busca mayor y menor nota

--- Modulo 2/ABP4 AE3/src/main/java/cl/lte/App.java
+++ Modulo 2/ABP4 AE3/src/main/java/cl/lte/App.java
@@ -1,10 +1,14 @@
 package cl.lte;
 
 /**
- * Hello world!
+ * Autor: Alejandra Flaño
+ * Fecha:22/7/2025
+ *
+ * Sistema notas de estudiantes, determina promedio y nota mayor y menor del curso
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CursoNotas curso = new CursoNotas();
+        curso.iniciar();
     }
 }
