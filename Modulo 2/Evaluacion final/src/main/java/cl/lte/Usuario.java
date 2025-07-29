14a75ad5d481e8fd9f7558e4cb8507127778d20a
Javier Rojas <rojas.javier.dev@gmail.com>
2025-07-29 19:13:19 -0400

    Se realizan las validaciones para la clase Usuario

--- Modulo 2/Evaluacion final/src/main/java/cl/lte/Usuario.java
+++ Modulo 2/Evaluacion final/src/main/java/cl/lte/Usuario.java
@@ -4,16 +4,15 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
-
 public class Usuario implements Asesoria {
     private String nombre;
     private String fechaNacimiento;
     private int run;
 
     public Usuario(String nombre, String fechaNacimiento, int run) {
-        this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        this.run = run;
+        setNombre(nombre);
+        setFechaNacimiento(fechaNacimiento);
+        setRun(run);
     }
 
     public int getRun() {
@@ -28,7 +27,7 @@ public class Usuario implements Asesoria {
     }
 
     // Lógica para calcular y mostrar la edad del usuario
-    public String mostrarEdad(){
+    public String mostrarEdad() {
         // Asumiendo que fechaNacimiento está en formato "dd/MM/yyyy"
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         // Aquí se debería calcular la edad basándose en la fecha de nacimiento
@@ -36,6 +35,32 @@ public class Usuario implements Asesoria {
         int edad = Period.between(fechaNac, LocalDate.now()).getYears();
         // Retornar la edad como un String
         return "El usuario tiene " + edad + " años.";
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre != null && nombre.length() >= 10 && nombre.length() <= 50) {
+            this.nombre = nombre;
+        } else {
+            System.out.println("Nombre debe tener entre 10 y 50 caracteres.");
+        }
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        // Validar formato de fecha (dd/MM/yyyy)
+        if (fechaNacimiento != null
+                && fechaNacimiento.matches("^([0][1-9]|[12][0-9]|3[01])/([0][1-9]|1[0-2])/\\d{4}$")) {
+            this.fechaNacimiento = fechaNacimiento;
+        } else {
+            System.out.println("Fecha de nacimiento debe estar en formato dd/MM/yyyy.");
+        }
+    }
+
+    public void setRun(int run) {
+        if (run < 99999999 && run > 0) {
+            this.run = run;
+        } else {
+            System.out.println("RUN debe ser un número válido menor a 99999999.");
+        }
     }
 
 }
