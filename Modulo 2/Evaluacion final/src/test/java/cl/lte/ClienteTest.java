96b713c547565d6a809aeab995317d6e1cbb63a9
IT-LTE <it-lte@latiaencanto.cl>
2025-08-02 08:42:29 -0400

    AFG 8 Creo test para validacion de nombre

--- Modulo 2/Evaluacion final/src/test/java/cl/lte/ClienteTest.java
+++ Modulo 2/Evaluacion final/src/test/java/cl/lte/ClienteTest.java
@@ -1,17 +1,19 @@
 package cl.lte;
 
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClienteTest {
+public class ClienteTest {
 
     @Test
     void testObtenerNombre() {
-        Cliente cliente = new Cliente("Juan Pérez", "01/01/1990", 12345678,
-                87654321, "Juan", "Pérez", "912345678", "Habitat", 1,
-                "Calle Falsa 123", "Santiago", 35);
+        Cliente cliente = new Cliente("Alejandra Flano", "01/01/1990", 12345678,
+                8674141, "Alejandra", "Flano", "912345678", "Habitat", 1,
+                "Calle Iberia 123", "Vina del mar", 80);
 
         String nombreCompleto = cliente.obtenerNombre();
-        assertEquals("Juan Pérez", nombreCompleto); // Espera "Juan Pérez"
+        assertEquals("Alejandra Flano", nombreCompleto); // Espera "Alejandra Flano"
     }
+
 }
