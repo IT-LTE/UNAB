2c654676b7fa38169b7a7cdf679f7fda489e32ba
IT-LTE <it-lte@latiaencanto.cl>
2025-08-06 00:50:35 -0400

    se crea Carpeta en Modulo 2 para realizar actividad ABP AE4

---
+++ Modulo 2/ABP AE4/src/main/java/cl/lte/App.java
@@ -0,0 +1,12 @@
+package cl.lte;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        GestionTareas gestor = new GestionTareas();
+        gestor.mostrarMenu();
+    }
+    }
+}
